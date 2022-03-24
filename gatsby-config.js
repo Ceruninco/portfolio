3d0fffcf3af591dcc60f70075ea1dcbe4c88e7f5
Ceruninco <48211045+Ceruninco@users.noreply.github.com>
2022-03-23 20:44:25 -0400

    metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Julien Nicolas',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'Julien Nicolas is a software engineer m passionate about Data Science, Algorithms and Software Development.',
+    siteUrl: 'https://upbeat-wozniak-3fe3b9.netlify.app/', // No trailing slash allowed!
+    image: '/cv.jpg', // Path to your image you placed in the 'static' folder
+    twitterUsername: '@jul_nicol',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
